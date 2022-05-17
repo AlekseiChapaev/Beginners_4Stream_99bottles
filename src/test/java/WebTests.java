5db3ca3503d9ec25d9e88c798a09966cb4a797db
Aleksei Chapaev <alekseiachapaev@gmail.com>
2022-05-17 19:50:50 +0200

    create new repo and add testCheckHeader

---
+++ src/test/java/WebTests.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class WebTests {
+
+    private final static String URL = "http://www.99-bottles-of-beer.net/";
+
+    @Test
+    public void testCheckHeader(){
+       System.setProperty("webdriver.chrome.driver", "C:/QA/4_stream/chromedriver.exe");
+       WebDriver driver = new ChromeDriver();
+
+       driver.get(URL);
+       driver.manage().window().maximize();
+
+       Assert.assertEquals(driver.findElement(By.xpath("//h1")).getText(), "99 Bottles of Beer");
+
+       driver.close();
+    }
+
+}
